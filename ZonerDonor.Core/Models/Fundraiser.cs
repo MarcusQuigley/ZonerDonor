5fd6f83204b4ff90562828d5f005d3948c9fbc80
marcus quigley <marcusquigley@gmail.com>
2020-02-01 16:09:36 -0500

    removing decimal warnings

--- ZonerDonor.Core/Models/Fundraiser.cs
+++ ZonerDonor.Core/Models/Fundraiser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ZonerDonor.Core.Models
 {
@@ -6,7 +7,9 @@ namespace ZonerDonor.Core.Models
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal Amount { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal CurrentTotal { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
     }
